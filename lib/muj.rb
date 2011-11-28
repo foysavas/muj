21fb7026ee0aacd6d6e1a36640312fa9248bad20
Foy Savas <foy@sav.as>
2011-11-28 11:24:03 -0500

    partials for y'all

--- lib/muj.rb
+++ lib/muj.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'tilt' unless defined? Tilt
 require 'json' unless defined? JSON
 
@@ -17,24 +18,43 @@ module Tilt
 end
 
 module Muj
+  class Partials
+    def initialize(dir)
+      @dir = File.expand_path(dir)
+    end
+    
+    def __exists(name)
+      fname = name.gsub('>','/')
+      File.exists?("#{@dir}/#{fname}.muj")
+    end
 
-  def self.eval(str,json)
+    def __read(name)
+      fname = name.gsub('>','/')
+      f = File.open("#{@dir}/#{fname}.muj")
+      r = f.read.chomp
+      f.close
+      r
+    end
+  end
+
+  def self.eval(str,json,opts={})
     require 'v8' unless defined? ::V8
     cxt = ::V8::Context.new
     cxt.load(File.dirname(__FILE__)+"/mustache.js")
-    cxt.eval(%Q{Mustache.to_html("#{str.strip.gsub("\n",'\n\\').gsub('"','\"')}", #{json});})
+    opts['partials'] = '.' unless opts['partials']
+    cxt['partials'] = Muj::Partials.new(opts['partials'])
+    cxt.eval(%Q{Mustache.to_html("#{str.chomp.gsub("\n",'\n\\').gsub('"','\"')}", #{json}, partials);})
   end
 
-  def self.render(str,locals={})
-    self.eval(str,locals.to_json)
+  def self.render(str,locals={},opts={})
+    self.eval(str,locals.to_json,opts)
   end
 
-  def self.render_with_json(str,json)
-    self.eval(str,json)
+  def self.render_with_json(str,json,opts={})
+    self.eval(str,json,opts)
   end
 
-  def self.render_with_yaml(str,yml)
-    require 'yaml' unless defined? YAML
-    self.render(str,YAML.load(yml))
+  def self.render_with_yaml(str,yml,opts={})
+    self.render(str,YAML::load(yml),opts)
   end
 end
--- test/init.rb
+++ test/init.rb
@@ -1,25 +1,43 @@
-FILE_DIRNAME = File.dirname(__FILE__)
-$:.unshift FILE_DIRNAME
-$:.unshift FILE_DIRNAME+"/../lib"
+FILE_DIR = File.dirname(__FILE__)
+FIXTURE_DIR = FILE_DIR + "/fixtures"
+$:.unshift FILE_DIR
+$:.unshift FILE_DIR+"/../lib"
 
 require 'rubygems'
 require 'test/unit'
 require 'lib/muj'
 
 class TestMuj < Test::Unit::TestCase
+  def setup
+    @data1 = {:user => {:first_name => "Sophia"}}
+    @tmpl1_fn = FIXTURE_DIR+"/test.muj"
+    @tmpl1 = File.open(@tmpl1_fn).read
+    @tmpl2 = File.open(FIXTURE_DIR+"/test-partials.muj").read
+  end
+
   def test_tilt
-    tmpl = Tilt.new(FILE_DIRNAME+"/test.muj")
+    tmpl = Tilt.new(@tmpl1_fn)
     r = tmpl.render(nil,{:user => {:first_name => "Sophia"}})
     assert_equal(r, "Hi Sophia!")
   end
 
-  def test_muj 
-    r = Muj.render(File.open(FILE_DIRNAME+"/test.muj").read,{:user => {:first_name => "Sophia"}})
+  def test_muj
+    r = Muj.render(@tmpl1,@data1)
     assert_equal(r, "Hi Sophia!")
   end
 
   def test_muj_with_yaml
-    r = Muj.render_with_yaml(File.open(FILE_DIRNAME+"/test.muj").read,"{'user': {'first_name': 'Sophia'}}")
+    r = Muj.render_with_yaml(@tmpl1,"{'user': {'first_name': 'Sophia'}}")
+    assert_equal(r, "Hi Sophia!")
+  end
+
+  def test_partials
+    partials = Muj::Partials.new(FIXTURE_DIR)
+    assert_equal(true,partials.__exists("test"))
+  end
+
+  def test_with_partials
+    r = Muj.render(@tmpl2,@data1,{'partials' => FIXTURE_DIR})
     assert_equal(r, "Hi Sophia!")
   end
 end
